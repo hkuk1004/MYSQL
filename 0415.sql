b708583752d58ed5967c1c45d4cb2859d3a6e7a6
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-15 12:45:56 +0900

    Add files via upload

--- 0415.sql
+++ 0415.sql
@@ -43,3 +43,23 @@ select ename, sal from emp where sal = max(sal);
 select max(sal) from emp;
 -- 먼저 실행할 문장을 괄호에 넣어서 실행 sub query
 select ename, sal from emp where sal=(select max(sal) from emp);
+-- emp 테이블에서
+-- 1. 회사 평균급여보다 급여를 많이 받는 사람의 이름, 급여
+select ename, sal from emp where sal > (select avg(sal) from emp);
+-- 2. 자기 부서 평균급여보다 급여를 많이 받는 사람의 이름, 급여
+select ename, sal from emp e where sal > (select avg(sal) from emp
+	where e.deptno = deptno);
+-- 3. 자기 부서 평균급여보다 급여를 많이받는 사람의 이름, 급여, 부서평균급여(round)
+-- in_line view에서 함수로 생성된 값은 반드시 별칭을 사용해야 한다
+select ename, sal, avgSal from emp e, (select deptno, round(avg(sal)) avgSal
+	from emp group by deptno) v where e.deptno = v.deptno and sal > avgSal;
+-- 4. 입사가 가장 빠른 사람의 이름, 입사일
+select ename, hiredate from emp where hiredate = (select min(hiredate) from emp);
+-- 5. 인천에서 근무하는 사람의 이름, 급여, 부서명, 근무지 급여큰순 정렬
+select ename, sal, dname, loc from emp e, dept d where e.deptno=d.deptno and
+	loc='인천' order by sal desc;
+-- 6. 사번, 이름, 급여, 부서명, 근무지 부서명순 부서명이 같으면 급여 큰순 정렬
+select empno, ename, sal, dname, loc from emp e, dept d where e.deptno=d.deptno
+	order by dname, sal desc;
+-- 7. 업무별 급여합계, 최대급여, 최소급여
+select job, sum(sal), max(sal), min(sal) from emp group by job;
